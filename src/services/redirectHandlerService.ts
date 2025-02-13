6417cd4c7871f2f80ba255b8d37212d61223d750
Joseph Charles <30187981+j027@users.noreply.github.com>
2025-02-12 21:08:23 -0500

    completely move reporting functionality off to separate functions to clean up code

--- src/services/redirectHandlerService.ts
+++ src/services/redirectHandlerService.ts
@@ -5,70 +5,67 @@ import { readConfig } from "../config";
 import { RedirectType } from "../redirectType";
 import { userAgentService } from "./userAgentService";
 
-export async function reportSite(
-  site: string,
-  redirect: string,
-) {
-  const {
-    channelId,
-    netcraftReportEmail,
-    urlscanApiKey,
-    netcraftReportSource,
-    proxy,
-  } = await readConfig();
-
-  // report to netcraft, google safe browsing, and urlscan.io
+async function reportToGoogleSafeBrowsing(site: string) {
+  const { proxy } = await readConfig();
   const proxyAgent = new ProxyAgent(proxy);
-  const reports = [];
-  reports.push(
-    fetch(
-      "https://safebrowsing.google.com/safebrowsing/clientreport/crx-report",
-      {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify([site]),
-        dispatcher: proxyAgent,
-      },
-    ),
-  );
-  reports.push(
-    fetch("https://report.netcraft.com/api/v3/report/urls", {
+  await fetch(
+    "https://safebrowsing.google.com/safebrowsing/clientreport/crx-report",
+    {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        email: netcraftReportEmail,
-        source: netcraftReportSource,
-        urls: [{ url: site }],
-      }),
+      body: JSON.stringify([site]),
       dispatcher: proxyAgent,
-    }),
+    },
   );
-  reports.push(
-    fetch("https://urlscan.io/api/v1/scan/", {
-      method: "POST",
-      headers: {
-        "API-Key": urlscanApiKey,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        url: site,
-        visibility: "public",
-      }),
+}
+
+async function reportToNetcraft(site: string) {
+  const { netcraftReportEmail, netcraftReportSource, proxy } =
+    await readConfig();
+  const proxyAgent = new ProxyAgent(proxy);
+  await fetch("https://report.netcraft.com/api/v3/report/urls", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      email: netcraftReportEmail,
+      source: netcraftReportSource,
+      urls: [{ url: site }],
     }),
-  );
+    dispatcher: proxyAgent,
+  });
+}
+
+async function reportToUrlscan(site: string) {
+  const { urlscanApiKey } = await readConfig();
+  await fetch("https://urlscan.io/api/v1/scan/", {
+    method: "POST",
+    headers: {
+      "API-Key": urlscanApiKey,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      url: site,
+      visibility: "public",
+    }),
+  });
+}
+
+export async function reportSite(site: string, redirect: string) {
+  // report to netcraft, google safe browsing, and urlscan.io
+  const reports = [];
+  reports.push(reportToGoogleSafeBrowsing(site));
+  reports.push(reportToNetcraft(site));
+  reports.push(reportToUrlscan(site));
 
   // send a message in the discord server with a link to the popup
-  reports.push(sendMessageToDiscord(channelId, site, redirect));
+  reports.push(sendMessageToDiscord(site, redirect));
 
   // wait for all the reports to finish
   await Promise.allSettled(reports);
 }
 
-async function sendMessageToDiscord(
-  channelId: string,
-  site: string,
-  redirect: string,
-) {
+async function sendMessageToDiscord(site: string, redirect: string) {
+  const { channelId } = await readConfig();
   const channel = discordClient.channels.cache.get(channelId) as TextChannel;
   if (channel) {
     await channel.send(`Found new popup with url ${site} from ${redirect}`);
@@ -83,7 +80,7 @@ export async function handleRedirect(
   regex: RegExp,
   redirectType: RedirectType,
 ): Promise<[string | null, boolean]> {
-  let location: string | null  = null;
+  let location: string | null = null;
 
   switch (redirectType) {
     case RedirectType.HTTP:
@@ -97,16 +94,14 @@ export async function handleRedirect(
   return [location, location != null ? regex.test(location) : false];
 }
 
-async function httpRedirect(
-  redirectUrl: string,
-): Promise<string | null> {
+async function httpRedirect(redirectUrl: string): Promise<string | null> {
   const { proxy } = await readConfig();
   const proxyAgent = new ProxyAgent(proxy);
 
   // fail hard if the user agent is not available - this ensures this is properly fixed
   const userAgent = await userAgentService.getUserAgent();
   if (userAgent == null) {
-      throw new Error("Failed to get user agent");
+    throw new Error("Failed to get user agent");
   }
 
   // check redirect through proxy
